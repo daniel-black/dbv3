271e06180613b53f19b2d34d1b78888a9d08ceb2
daniel-black <drblack651@gmail.com>
2023-01-02 12:12:42 -0700

    work on home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
-      <body>{children}</body>
+      <body className='bg-zinc-900 text-zinc-50 py-20 px-6'>
+        <h1 className='text-4xl font-bold mb-10'>
+          Dan Black
+        </h1>
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,44 @@
+const listItems = [
+  {
+    emoji: '❄️',
+    text: 'I\'m from Minnesota',
+  },
+  {
+    emoji: '🏔',
+    text: 'I live in Denver',
+  },
+  {
+    emoji: '💻',
+    text: 'Studied computer engineering at the University of Minnesota (graduated in 2022)',
+  },
+  {
+    emoji: '👨‍💻',
+    text: 'Work as a software engineer for C.H. Robinson on an app called Freightquote',
+  },
+  {
+    emoji: '📚',
+    text: 'I like to read',
+  },
+  {
+    emoji: '🙅‍♂️',
+    text: 'And I abhor cooking',
+  },
+];
 
 export default function Home() {
   return (
-   <div>home page</div>
+    <div className='space-y-5'>
+      <p>Hello there 👋</p>
+      <p>My name is Dan and this is my home on the internet.</p>
+      <p>Here's a couple quick things about me:</p>
+      <ul className='space-y-2'>
+        {listItems.map(item => (
+          <li key={item.emoji} className='flex flex-row space-x-2'>
+            <span>{item.emoji}</span>
+            <span>{item.text}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
