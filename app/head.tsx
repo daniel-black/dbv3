a1f5d79a0230dfa5ef4b346c03e39b42550a4495
daniel-black <drblack651@gmail.com>
2023-01-03 19:59:00 -0700

    remove pages/api and add favicon

--- app/head.tsx
+++ app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>Dan Black</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Dan Black's personal website" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🦄</text></svg>"></link>
     </>
   );
 }
