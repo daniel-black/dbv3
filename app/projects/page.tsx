955bbd1e4c9c6743b797c0be5b7d0080da02f98e
daniel-black <drblack651@gmail.com>
2023-09-27 00:39:26 -0600

    update projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,24 +1,66 @@
-
 export default function ProjectsPage() {
   return (
-    <div className='space-y-5 text-lg text-zinc-400'>
-      <p>In 2023, I'm trying to finish the things that I start. My Github has entirely too many half-baked projects laying around... 😅</p>
-      <p>I like messing around with TypeScript/JavaScript, React, Next.js, and Tailwind.</p>
-      <p>One of my goals this year is to pick up one of the popular, more performant languages like Go or Rust. We'll see how that goes : )</p>
-      <ul className='space-y-2 list-disc ml-5'>
+    <div className="space-y-5 text-lg text-zinc-400">
+      <p>
+        In 2023, I'm trying to finish the things that I start. My Github has
+        entirely too many half-baked projects laying around... 😅
+      </p>
+      <p>
+        I like messing around with TypeScript/JavaScript, React, Next.js, and
+        Tailwind.
+      </p>
+      <p>
+        One of my goals this year is to pick up one of the popular, more
+        performant languages like Go or Rust. We'll see how that goes :)
+      </p>
+      <ul className="space-y-2 list-disc ml-5">
         <li>
-          <a href="https://textmachina.vercel.app/" target='_blank' className='link'>Text Machina</a> ChatGPT but with style
+          <a
+            href="https://refactory-ochre.vercel.app/"
+            target="_blank"
+            className="link"
+          >
+            Refactory
+          </a>{" "}
+          - Refactor code with the help of AI
         </li>
         <li>
-          <a href="https://promocha-v2.vercel.app" target='_blank' className='link'>ProMocha</a> A tool for managing promocodes
+          <a
+            href="https://textmachina.vercel.app/"
+            target="_blank"
+            className="link"
+          >
+            Text Machina
+          </a>{" "}
+          - ChatGPT but with style
         </li>
         <li>
-          <a href='https://tack-weld.vercel.app' target='_blank' className='link'>Tack</a> - A tool for building TailwindCSS color palettes
+          <a
+            href="https://promocha-v2.vercel.app"
+            target="_blank"
+            className="link"
+          >
+            ProMocha
+          </a>{" "}
+          - A tool for managing promocodes
         </li>
         <li>
-          <a href='https://www.hacolyte.com' target='_blank' className='link'>Hacolyte</a> - A client for reading Hacker News stories
+          <a
+            href="https://tack-weld.vercel.app"
+            target="_blank"
+            className="link"
+          >
+            Tack
+          </a>{" "}
+          - A tool for building TailwindCSS color palettes
         </li>
         <li>
+          <a href="https://www.hacolyte.com" target="_blank" className="link">
+            Hacolyte
+          </a>{" "}
+          - A client for reading Hacker News stories
+        </li>
+        {/* <li>
           <a href="https://where-to-eta.vercel.app" target='_blank' className='link'>Where To?</a> - A very incomplete move-planning app
         </li>
         <li>
@@ -26,7 +68,7 @@ export default function ProjectsPage() {
         </li>
         <li>
           <a href="https://yallhiring.vercel.app" target='_blank' className='link'>Y'all Hiring?</a> - An incomplete job board for US Gov jobs 👷‍♂️
-        </li>
+        </li> */}
       </ul>
     </div>
   );
